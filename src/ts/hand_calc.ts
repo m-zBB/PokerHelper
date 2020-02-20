17c0afa82c7fe7eb0ed7f70b64fd4d7e09b430ec
Daniel Juszczuk <juszczuk.dan@gmail.com>
2020-02-20 17:56:36 +0100

    use browserify with watchify and tsify plugins to build bundle

--- src/ts/hand_calc.ts
+++
@@ -1,6 +0,0 @@
-export function calculate(event: KeyboardEvent) {
-    if (event.keyCode === 13) {
-        var x = <HTMLInputElement>document.getElementById("hand");
-        alert(x.value);
-    }
-}
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,8 +7,20 @@
 
 const hutchison = require('hutchison');
 
-function greeter() {
-  console.log(hutchison.texasHoldem({hand: ['Ah', 'Ad']}));
+function calculate(event: KeyboardEvent) {
+  if (event.keyCode === 13) {
+      var x = <HTMLInputElement>document.getElementById("hand");
+      alert(x.value);
+  }
 }
 
-window.onload = greeter
+var input = document.getElementById("hand");
+if (input) {
+  input.onkeyup = calculate;
+}
+
+
+// function greeter() {
+//   console.log(hutchison.texasHoldem({hand: ['Ah', 'Ad']}));
+// }
+// window.onload = greeter
