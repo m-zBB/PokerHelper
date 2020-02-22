758491c1ad940f3fa7aff741a73f282d7d039b3c
Daniel Juszczuk <juszczuk.dan@gmail.com>
2020-02-22 18:34:09 +0100

    set indents to 4 spaces, install hutchison lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { texasHoldem } from 'hutchison';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'poker-helper';
+export class AppComponent implements OnInit {
+    title = 'poker-helper';
+
+    ngOnInit() {
+        var result = texasHoldem({ hand: ['Ah', 'Ad'] });
+        //alert(result.percentile * 100);
+    }
 }
